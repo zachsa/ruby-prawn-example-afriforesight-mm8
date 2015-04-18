60522d5a9248f7176f69063d5dc8d4e54e0bff00
Zach Smith <zachsmith.sa@gmail.com>
2015-04-18 17:23:49 +0200

    no change

--- lib/db/db_api.rb
+++ lib/db/db_api.rb
@@ -1,1 +1,27 @@
+def add_to_db
+	db = Mysql2::Client.new(:host => "localhost", :username => "root", :password => 'password', :database => 'mm8')
 
+	data.each do |k,v|
+		v.each do |c|
+			commodity = k.to_s.sub("_", " ").capitalize
+			if commodity == "Oil gas"
+				commodity = "Oil & Gas"
+			end
+		
+			date = Date.strptime("2015-03-09")
+		
+			country = c[0]
+			story = c[1]
+		
+		
+			sql = "INSERT INTO stories SET
+					Date = \"#{date}\",
+					Category = 'mm8',
+					Country = \"#{country}\",
+					Commodity = \"#{commodity}\",
+					Text = \"#{story}\";"
+		
+			db.query(sql)
+		end
+	end
+end
