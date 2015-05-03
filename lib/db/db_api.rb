9170f46a5bc0386a3088314725893b3cf45c9ff8
Zach Smith <zachsmith.sa@gmail.com>
2015-05-03 20:07:32 +0200

    start sun evening

--- lib/db/db_api.rb
+++ lib/db/db_api.rb
@@ -1,38 +1,57 @@
 require 'Mysql2'
-require 'win32ole'
+require 'os'
+
+require 'win32ole' if OS.windows?
 
 
+class DB
+  def initialize(host, username, password, database, data)
+    @host = host
+    @username = username
+    @password = password
+    @database = database
+    @data = data    
+  end
 
-def add_to_db(data)
-	db = Mysql2::Client.new(:host => "localhost", :username => "root", :password => 'pfnafn1', :database => 'afriforesightresearch')
-
-
-
-	data.each do |k,v|
-		v.each do |c|
-			commodity = k.to_s.sub("_", " ").capitalize
-			if commodity == "Oil gas"
-				commodity = "Oil & Gas"
-			end
+  def add_to_mysql_db
+    begin
+      db = MySQL2::Client.new(:host => @host, :username => @username, :password => @password, :database => @database)
+      success = true
+    rescue
+      puts "ERROR: Unable to connect to MySQL database"
+      success = false
+    end
+    
+    if success
+    	@data.each do |k,v|
+    		v.each do |c|
+  			
+          commodity = k.to_s.sub("_", " ").capitalize
+    			if commodity == "Oil gas"
+    				commodity = "Oil & Gas"
+    			end
 		
-			date = Date.strptime("2015-03-09")
+    			date = Date.strptime("2015-03-09")
 		
-			country = c[0]
-			story = c[1]
+    			country = c[0]
+    			story = c[1]
 
 		
-			sql = "REPLACE INTO mm8 SET
-					story_date = \"#{date}\",
-					Category = 'mm8',
-					Country = \"#{country}\",
-					Commodity = \"#{commodity}\",
-					story_text = \"#{story}\",
-					unique_text = MD5(\"#{story}\");"
+    			sql = "REPLACE INTO mm8 SET
+    					story_date = \"#{date}\",
+    					Category = 'mm8',
+    					Country = \"#{country}\",
+    					Commodity = \"#{commodity}\",
+    					story_text = \"#{story}\",
+    					unique_text = MD5(\"#{story}\");"
 
-			db.query(sql)
+    			db.query(sql)
 
-		end
-	end
+    		end
+    	end
+  end
+  end
+
 end
 
 =begin
